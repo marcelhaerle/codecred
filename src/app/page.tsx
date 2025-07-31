69ad99b996b935d8dc3af945e5c64002e79707eb
Marcel Härle <marcel.haerle@sonnvest.de>
2025-07-31 11:30:08 +0200

    refactor: move marketing component to saas related components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import LoginPrompt from "@/components/LoginPrompt";
-import MarketingSections from "@/components/MarketingSections";
+import MarketingSections from "@/components/saas/MarketingSections";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 
