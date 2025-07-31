2290a2708af3e7c79f8f15a1513e25295386adb8
Marcel Härle <marcel.haerle@sonnvest.de>
2025-07-31 11:03:20 +0200

    feat: replace logout button with drop-down menu

--- src/components/AuthHeader.tsx
+++ src/components/AuthHeader.tsx
@@ -2,11 +2,21 @@
 
 import { useSession } from "next-auth/react";
 import Link from "next/link";
-
-import LogoutButton from "./LogoutButton";
+import { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
+import { signOut } from "next-auth/react"
+import { ChevronDown, ChevronUp, User } from "lucide-react";
 
 export default function AuthHeader() {
   const { data: session } = useSession();
+  const pathname = usePathname();
+
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    // Close the dropdown if the user navigates to a different page
+    setOpen(false);
+  }, [pathname]);
 
   return (
     <header className="fixed top-0 left-0 right-0 z-50 bg-gray-950/80 backdrop-blur-sm border-b border-gray-800">
@@ -24,9 +34,26 @@ export default function AuthHeader() {
           <Link href="/auth/links" className="text-gray-400 hover:text-white transition-colors">Links</Link>
           <Link href="/auth/rss" className="text-gray-400 hover:text-white transition-colors">RSS</Link>
           <Link href="/auth/projects" className="text-gray-400 hover:text-white transition-colors">Projects</Link>
-          <Link href={`/${session?.user.username}`} target="_blank" className="text-gray-400 hover:text-white transition-colors flex group">Profile</Link>
         </nav>
-        <LogoutButton username={session?.user.name} />
+        <div className="relative group">
+          <button
+            className="text-gray-400 hover:text-white transition-colors flex items-center gap-2"
+            onClick={() => setOpen(!open)}
+          >
+            {session?.user.name}
+            {open ? <ChevronUp /> : <ChevronDown />}
+          </button>
+          <div
+            className="absolute right-0 mt-4 z-10 w-60 bg-gray-800 rounded-md shadow-lg px-4 py-4 text-center"
+            style={{ display: open ? 'block' : 'none' }}
+          >
+            <Link href="/auth/account" className="block px-4 py-2 text-white hover:bg-gray-700 rounded">
+              <User className="inline mr-2" size={20} /> Account
+            </Link>
+            <hr className="my-4 border-gray-700" />
+            <button className="bg-blue-800 hover:bg-blue-700 text-white px-4 py-2 rounded w-full" onClick={() => signOut()}>Sign out</button>
+          </div>
+        </div>
       </div>
     </header>
   );
